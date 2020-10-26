7c0e4312fb8041ebc9675b82eb2546f5d1510db8
LukaRaguz <lraguz@foi.hr>
2020-10-26 17:49:19 +0100

    Napravljeno sučelje za registraciju.

--- MenzApp/app/src/main/java/hr/foi/air2003/menzapp/RegistrationActivity.kt
+++ MenzApp/app/src/main/java/hr/foi/air2003/menzapp/RegistrationActivity.kt
@@ -2,11 +2,10 @@ package hr.foi.air2003.menzapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registration)
     }
 }
