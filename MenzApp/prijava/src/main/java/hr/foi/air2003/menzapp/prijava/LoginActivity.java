7c5889ce53072a89e1dd8a666af8528cc963c0f0
EnaLulic <elulic@foi.com>
2020-11-04 18:57:28 +0100

    Added design for case of forgotten password and modified login interface design.

--- MenzApp/prijava/src/main/java/hr/foi/air2003/menzapp/prijava/LoginActivity.java
+++
@@ -1,4 +0,0 @@
-package hr.foi.air2003.menzapp.prijava;
-
-public class LoginActivity {
-}
---
+++ MenzApp/prijava/src/main/java/hr/foi/air2003/menzapp/prijava/LoginActivity.kt
@@ -0,0 +1,22 @@
+package hr.foi.air2003.menzapp.prijava
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.dialog_password.view.*
+import kotlinx.android.synthetic.main.login_main.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_main)
+
+
+    }
+
+}
