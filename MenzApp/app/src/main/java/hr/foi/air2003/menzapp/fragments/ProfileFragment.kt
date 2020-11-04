3d0b6fad72543c072abc17bb9d1eb59e6b87042a
ivapapac <ipapac@foi.hr>
2020-11-04 10:37:58 +0100

    Added two card views into profile fragment. First one for user's posts and second one for feedbacks. Profile design finished.

--- MenzApp/app/src/main/java/hr/foi/air2003/menzapp/fragments/ProfileFragment.kt
+++ MenzApp/app/src/main/java/hr/foi/air2003/menzapp/fragments/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import hr.foi.air2003.menzapp.R
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
