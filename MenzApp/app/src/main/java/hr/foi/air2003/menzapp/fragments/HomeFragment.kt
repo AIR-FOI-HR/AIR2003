b21c4e3ad219e1bb2374b3f6127c249799274bce
EnaLulic <elulic@foi.com>
2020-11-10 15:09:21 +0100

    Design for new post and modified design for edit post. Edit post was made as a pop-up window.

--- MenzApp/app/src/main/java/hr/foi/air2003/menzapp/fragments/HomeFragment.kt
+++ MenzApp/app/src/main/java/hr/foi/air2003/menzapp/fragments/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import hr.foi.air2003.menzapp.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -14,6 +16,13 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        /*PROVJERITI
+        btnNewPost.setOnClickListener{
+            Toast.makeText(context, "Nova objava", Toast.LENGTH_LONG).show()
+        }*/
+
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+
     }
 }
--- MenzApp/app/src/main/java/hr/foi/air2003/menzapp/fragments/ProfileFragment.kt
+++ MenzApp/app/src/main/java/hr/foi/air2003/menzapp/fragments/ProfileFragment.kt
@@ -1,10 +1,13 @@
 package hr.foi.air2003.menzapp.fragments
 
 import android.app.Activity
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.transition.Explode
 import androidx.transition.TransitionManager
@@ -31,7 +34,19 @@ class ProfileFragment : Fragment() {
                TransitionManager.beginDelayedTransition(view.cvMyPosts, Explode())
                view.expandablePosts.visibility = View.GONE
            }
+
+           // Edit post
+           btnEditPost.setOnClickListener{
+               val popupEditPost = Dialog(view.context)
+               popupEditPost.setContentView(layoutInflater.inflate(R.layout.dialog_edit_post,null))
+
+               popupEditPost.show()
+
+
+           }
+
        }
+
 
        view.cvFeedback.setOnClickListener {
            if(view.expandableFeedbacks.visibility == View.GONE){
