719ce57f6111d57dab7a8b4d8c22a9ae781b7dac
Pero Radić <peroradic8@gmail.com>
2020-11-27 22:56:42 +0100

    Design bugs fixed.

--- MenzApp/app/src/main/java/hr/foi/air2003/menzapp/MainActivity.kt
+++ MenzApp/app/src/main/java/hr/foi/air2003/menzapp/MainActivity.kt
@@ -13,51 +13,65 @@ import hr.foi.air2003.menzapp.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: MainViewModel
+    private var viewModel: MainViewModel = MainViewModel()
     private var currentUser: FirebaseUser? = null
     private lateinit var user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        currentUser = FirebaseAuth.getInstance().currentUser
+        if(currentUser === null) { userLogin() }
+
+        requireUserData()
     }
 
     override fun onStart() {
         super.onStart()
-        viewModel = MainViewModel()
-        user = User()
-
-        // Get current user
-        currentUser = FirebaseAuth.getInstance().currentUser
-        if(currentUser === null)
-            userLogin()
-        else
-            setUser()
-
-        val homeFragment = HomeFragment()
-        val profileFragment = ProfileFragment()
-        val menuFragment = MenuFragment()
-        val chatFragment = ChatFragment()
-        val searchFragment = SearchFragment()
 
         // Set current fragment when navigation icon is selected
-        setCurrentFragment(homeFragment)
         bottom_nav_bar.setOnNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.ic_user -> setCurrentFragment(profileFragment)
-                R.id.ic_search -> setCurrentFragment(searchFragment)
-                R.id.ic_food -> setCurrentFragment(menuFragment)
-                R.id.ic_chat -> setCurrentFragment(chatFragment)
-                R.id.ic_home -> setCurrentFragment(homeFragment)
+                R.id.ic_user -> setCurrentFragment(ProfileFragment())
+                R.id.ic_search -> setCurrentFragment(SearchFragment())
+                R.id.ic_food -> setCurrentFragment(MenuFragment())
+                R.id.ic_chat -> setCurrentFragment(ChatFragment())
+                R.id.ic_home -> setCurrentFragment(HomeFragment())
             }
             true
         }
     }
 
+    override fun onRestart() {
+        super.onRestart()
+        when (bottom_nav_bar.selectedItemId) {
+            R.id.ic_user -> setCurrentFragment(ProfileFragment())
+            R.id.ic_search -> setCurrentFragment(SearchFragment())
+            R.id.ic_food -> setCurrentFragment(MenuFragment())
+            R.id.ic_chat -> setCurrentFragment(ChatFragment())
+            R.id.ic_home -> setCurrentFragment(HomeFragment())
+        }
+    }
+
     private fun userLogin() {
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        setUser()
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    private fun requireUserData(){
+        if(currentUser != null){
+            val liveData = viewModel.getUser(currentUser!!.uid)
+            liveData.observe(this, {
+                val data = it.data
+                if(data != null)
+                    loadUserData(data)
+            })
+        }
+    }
+
+    private fun loadUserData(user: User){
+        this.user = user
+        setCurrentFragment(HomeFragment())
     }
 
     private fun setCurrentFragment(fragment: Fragment) {
@@ -69,22 +83,5 @@ class MainActivity : AppCompatActivity() {
 
         // Open fragment view inside of container
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
-    }
-
-    private fun setUser(){
-        if(currentUser != null){
-            val liveData = viewModel.getUser(currentUser!!.uid)
-            liveData.observe(this, {
-                val data = it.data
-                if(data != null) {
-                    user.userId = data.userId
-                    user.fullName = data.fullName
-                    user.bio = data.bio
-                    user.email = data.email
-                    user.profilePicture = data.profilePicture
-                    user.notificationsOn = data.notificationsOn
-                }
-            })
-        }
     }
 }
