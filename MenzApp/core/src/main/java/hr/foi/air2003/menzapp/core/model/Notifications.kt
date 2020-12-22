63d50c3dc6686e26b33e8e5396cae9301757400c
Pero Radić <peroradic8@gmail.com>
2020-12-22 16:37:05 +0100

    Notification model dataclass created

---
+++ MenzApp/core/src/main/java/hr/foi/air2003/menzapp/core/model/Notifications.kt
@@ -0,0 +1,15 @@
+package hr.foi.air2003.menzapp.core.model
+
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentId
+
+data class Notifications (
+    @DocumentId
+    var authorId: String = "",
+    var content: String = "",
+    var isRequest: Boolean = true,
+    var notificationId: String = "",
+    var postId: String = "",
+    var timestamp: Timestamp = Timestamp.now(),
+    var recipientsId: List<String> = listOf()
+)
