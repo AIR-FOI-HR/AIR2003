0156bcbe183d5faa18bcad1426c85bdd1a6beb46
EnaLulic <elulic@foi.com>
2021-01-14 18:46:47 +0100

    Implementation of checking Internet connection in splash screen.

--- MenzApp/app/src/main/java/hr/foi/air2003/menzapp/activities/SplashScreenActivity.kt
+++ MenzApp/app/src/main/java/hr/foi/air2003/menzapp/activities/SplashScreenActivity.kt
@@ -1,11 +1,20 @@
 package hr.foi.air2003.menzapp.activities
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import hr.foi.air2003.menzapp.R
 import hr.foi.air2003.menzapp.core.services.FirebaseAuthService
 import hr.foi.air2003.menzapp.login.LoginActivity
+import kotlinx.android.synthetic.main.dialog_internet.*
 
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var handler: Handler
@@ -16,12 +25,37 @@ class SplashScreenActivity : AppCompatActivity() {
         handler.postDelayed(runnable, 1000)
     }
 
+    private fun checkConnection():Boolean {
+        val manager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = manager.activeNetworkInfo
+        if(null!=networkInfo){
+            return true
+        }
+        else{
+           val dialog = Dialog(this)
+            dialog.setContentView(R.layout.dialog_internet)
+            dialog.setCanceledOnTouchOutside(false)
+            dialog.btn_checkConnection.setOnClickListener{
+                recreate()
+            }
+            dialog.show()
+            dialog.btn_cancel.setOnClickListener{
+                finish();
+                System.exit(0);
+            }
+            return false
+        }
+    }
+
     private val runnable = Runnable {
-        if (!isFinishing) {
-            if(FirebaseAuthService.getCurrentUser() === null)
-                startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
-            else
-                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        if(checkConnection()) {
+            if (!isFinishing) {
+                if (FirebaseAuthService.getCurrentUser() === null) {
+                    startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
+                } else {
+                    startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                }
+            }
         }
     }
 
@@ -34,4 +68,5 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onPause()
         handler.removeCallbacks(runnable)
     }
+
 }
