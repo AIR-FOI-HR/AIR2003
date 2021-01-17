aa60932c3a92ad4867de0217182779c5efd58463
Vicko Lukšić <vickoluksic@atron.de>
2021-01-17 15:48:47 +0100

    Create user in database if not present

--- MenzApp/login/src/main/java/hr/foi/air2003/menzapp/login/LoginActivity.kt
+++ MenzApp/login/src/main/java/hr/foi/air2003/menzapp/login/LoginActivity.kt
@@ -16,15 +16,20 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import hr.foi.air2003.menzapp.core.Repository
+import hr.foi.air2003.menzapp.core.model.User
 import kotlinx.android.synthetic.main.dialog_password.*
 import kotlinx.android.synthetic.main.login_main.*
 import kotlinx.android.synthetic.main.login_main.txtPassword
+import kotlinx.android.synthetic.main.registration_main.*
 
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private var repository = Repository()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,15 +45,15 @@ class LoginActivity : AppCompatActivity() {
         }
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
+                .requestIdToken(REQUEST_TOKEN)
+                .requestEmail()
+                .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         txtRegister.setOnClickListener {
             startActivity(Intent(this, RegistrationActivity::class.java))
         }
-
         txtForgotPassword.setOnClickListener {
             Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show()
 
@@ -87,7 +92,7 @@ class LoginActivity : AppCompatActivity() {
             try {
                 val account = task.getResult(ApiException::class.java)!!
                 Log.d(LoginActivity.TAG, "firebaseAuthWithGoogle:" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
+                firebaseAuthWithGoogle(account)
             } catch (e: ApiException) {
                 Log.w(LoginActivity.TAG, "Google sign in failed", e)
                 updateUI(null)
@@ -95,19 +100,20 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
         auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Log.d(LoginActivity.TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    Log.w(LoginActivity.TAG, "signInWithCredential:failure", task.exception)
-                    updateUI(null)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(LoginActivity.TAG, "signInWithCredential:success")
+                        val userInDatabase = repository.getUser(auth.currentUser?.uid!!)
+                        if (userInDatabase === null) {
+                            Log.d(LoginActivity.TAG, "user not in db, creating entry...")
+                            repository.createUser(auth.currentUser?.uid!!, getUserInfo(account, auth.currentUser?.uid!!))
+                        }
+                        super.onBackPressed()
+                    } else { Log.w(LoginActivity.TAG, "signInWithCredential:failure", task.exception) }
                 }
-            }
     }
 
     override fun onStart() {
@@ -165,10 +171,22 @@ class LoginActivity : AppCompatActivity() {
 
     private fun updateUI(user: FirebaseUser?) {
         // TODO add user successfully created message or something like that
+        if (user !== null && user.uid !== null) {
+            Log.d(LoginActivity.TAG, user?.uid!!)
+        }
+
     }
 
+    private fun getUserInfo(user: GoogleSignInAccount, firebaseUserId: String): User {
+        return User(
+                userId = firebaseUserId,
+                fullName = user.displayName!!,
+                email = user.email!!
+        )
+    }
     companion object {
         private const val TAG = "LoginActivity"
         private const val RC_SIGN_IN = 10000
+        private const val REQUEST_TOKEN = "211328840636-3ngg2a5uno5hofdbq6sbsqqb199arl1g.apps.googleusercontent.com"
     }
 }
